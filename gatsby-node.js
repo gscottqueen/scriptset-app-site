3fee36d7bf2d89ab063090e5396518deed314210
gscottqueen <geoffreysqueen@gmail.com>
2018-07-07 22:18:45 -0400

    removed syntax-async nod, updated our node hook

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,15 +3,10 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-// exports.modifyBabelrc = ({ babelrc }) => ({
-//   ...babelrc,
-//   plugins: babelrc.plugins.concat(
-//     ['transform-regenerator']),
-// })
-
 exports.modifyBabelrc = ({ babelrc }) => ({
   ...babelrc,
   plugins: babelrc.plugins.concat(
-    ['transform-runtime', 'transform-regenerator', 'syntax-async-functions']
+    ['transform-runtime'],
+    ['transform-regenerator']
   ),
 }) // to deal with our async issue on static files https://github.com/gatsbyjs/gatsby/issues/3931
