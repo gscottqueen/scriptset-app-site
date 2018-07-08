2fd27251998e64d1ea0038ab55aaed7bb5edee0b
gscottqueen <geoffreysqueen@gmail.com>
2018-07-08 08:27:54 -0400

    removing runtime from our node.js hook

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,6 @@
 exports.modifyBabelrc = ({ babelrc }) => ({
   ...babelrc,
   plugins: babelrc.plugins.concat(
-    ['transform-runtime'],
     ['transform-regenerator']
   ),
 }) // to deal with our async issue on static files https://github.com/gatsbyjs/gatsby/issues/3931
