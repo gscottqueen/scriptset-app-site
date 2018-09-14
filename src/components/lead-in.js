4e46999a8380b32cea8581d5aafa247081a6de7f
gscottqueen <geoffreysqueen@gmail.com>
2018-09-14 12:26:56 -0400

    new leadin component

---
+++ src/components/lead-in.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const leadInStyle = {  
+  "margin" : "100px 0 50px 0",
+  "textAlign" : "center",
+  "fontWeight" : "300",
+}
+
+const LeadIn = () => (
+  <h2 style={ leadInStyle }>A new web application to help screenwriters create and collaborate.</h2>
+)
+
+export default LeadIn
